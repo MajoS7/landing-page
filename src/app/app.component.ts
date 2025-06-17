6fa5d5600be1f34cbb34e931a46a81b249e2116c
Majo <majos2764@gmail.com>
2025-06-16 21:46:18 -0500

    FIX: Validations are added to the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { NgClass } from '@angular/common';
 import { Component,inject ,WritableSignal, signal} from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -15,16 +16,15 @@ export class AppComponent {
   cargando = signal(false);
   contactForm: FormGroup = this.fb.group({
     nombre: ['', Validators.required],
-    correo: ['', [Validators.required, Validators.email]],
-    mensaje: ['']
+    correo: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+    mensaje: ['', Validators.required]
   });
 
 
-  async onSubmit(event: SubmitEvent) {
+  public async onSubmit(event: SubmitEvent) {
     event.preventDefault();
     if (this.contactForm.valid) {
       const formData = this.contactForm.value;
-      console.log('Formulario enviado:', formData);
       this.cargando.set(true);
       const res = await fetch("/.netlify/functions/formulario-contacto", {
         method:'POST',
@@ -35,6 +35,13 @@ export class AppComponent {
       })
        this.cargando.set(false);
        this.exito.set(res.ok);
+    }else{
+      this.contactForm.markAllAsTouched();
     }
+  }
+
+   public campoInvalido(campo: string): boolean {
+    const control = this.contactForm.get(campo);
+    return control?.invalid && control?.touched || false;
   }
 }
