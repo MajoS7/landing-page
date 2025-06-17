c5eb5243fccf1febab42b574cd24b431aad1f16e
Majo <majos2764@gmail.com>
2025-06-16 20:30:23 -0500

    FEAT: Initial configuration and landing page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component,inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'landing-page';
+  fb: FormBuilder = inject(FormBuilder);
+  contactForm: FormGroup = this.fb.group({
+    nombre: ['', Validators.required],
+    correo: ['', [Validators.required, Validators.email]],
+    mensaje: ['']
+  });
+
+  exito = false;
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      const formData = this.contactForm.value;
+      console.log('Formulario enviado:', formData);
+      // Aquí puedes conectar con Firebase, EmailJS, etc.
+    }
+  }
 }
