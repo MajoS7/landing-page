9b1b0b8d3822f55a0a9eff037303ca61babb9303
Majo <majos2764@gmail.com>
2025-06-16 21:05:43 -0500

    FEAT: Added request to view form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component,inject } from '@angular/core';
+import { Component,inject ,WritableSignal, signal} from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -11,19 +11,29 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 export class AppComponent {
   title = 'landing-page';
   fb: FormBuilder = inject(FormBuilder);
+  exito:WritableSignal<boolean|undefined> = signal(undefined);;
+  cargando = signal(false);
   contactForm: FormGroup = this.fb.group({
     nombre: ['', Validators.required],
     correo: ['', [Validators.required, Validators.email]],
     mensaje: ['']
   });
 
-  exito = false;
 
-  onSubmit() {
+  async onSubmit() {
     if (this.contactForm.valid) {
       const formData = this.contactForm.value;
       console.log('Formulario enviado:', formData);
-      // Aquí puedes conectar con Firebase, EmailJS, etc.
+      this.cargando.set(true);
+      const res = await fetch("/.netlify/functions/formulario-contacto", {
+        method:'POST',
+        headers: {
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+       this.cargando.set(false);
+       this.exito.set(res.ok);
     }
   }
 }
